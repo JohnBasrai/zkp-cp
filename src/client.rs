16c9e8e64fdf13f5967cc9811aeebb241a09f5a2
John Basrai <John.Basrai@vts-i.com>
2024-09-26 19:39:15 -0700

    Reformatted with rustfmt
    - Converted from nightly build stable
    - Deleted some now unsupported settings in rustfmt.toml
    - Cleaned up some clippy warings and other code issues
    - Added documentation comments to the code

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,9 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
 use num_bigint::BigUint;
+use rpassword::prompt_password;
 
-pub mod zkp_auth
-{
+pub mod zkp_auth {
     include!("./zkp_auth.rs");
 }
 
@@ -16,15 +16,10 @@ use zkp_chaum_pedersen::ZKP;
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 /// Chaum-Pedersen Zero Knowledge Proof (client)
-struct Args
-{
+struct Args {
     /// User name
     #[arg(short, long, required = true)]
     user_name: String,
-
-    /// Password
-    #[arg(short, long, required = true)]
-    password: String,
 
     /// Server endpoint
     #[arg(
@@ -37,62 +32,56 @@ struct Args
 }
 
 #[tokio::main]
-async fn main() -> Result<(), String>
-{
-    let args = &Args::parse();
+async fn main() -> Result<()> {
+    let args = Args::parse();
 
     let (alpha, beta, p, q) = ZKP::get_constants();
     let zkp = ZKP::new(&p, &q, &alpha, &beta);
 
-    let mut client = match AuthClient::connect(args.server.clone()).await
-    {
+    let mut client = match AuthClient::connect(args.server.clone()).await {
         Ok(x) => x,
-        Err(e) =>
-        {
-            return Err(format!("Connect failed to {}: {:?}", args.server, e));
+        Err(e) => {
+            return Err(anyhow!("Failed to register user on server {}: {:?}", args.server, e));
         }
     };
 
     println!("✅ Connected to the server");
-    let password = BigUint::from_bytes_be(args.password.trim().as_bytes());
-
+    let password = prompt_password("Password: ")?;
+    let password = BigUint::from_bytes_be(password.trim().as_bytes());
     let (y1, y2) = zkp.compute_pair(&password);
 
     let username = args.user_name.clone();
     let request = RegisterRequest {
         user: username.clone(),
-        y1:   y1.to_bytes_be(),
-        y2:   y2.to_bytes_be(),
+        y1: y1.to_bytes_be(),
+        y2: y2.to_bytes_be(),
     };
 
-    let _ = match client.register(request).await
-    {
+    let _ = match client.register(request).await {
         Ok(x) => x,
-        Err(e) =>
-        {
-            return Err(format!("Could not register user name with server {:?}", e));
+        Err(e) => {
+            return Err(anyhow!("Could not register user name with server {:?}", e));
         }
     };
     println!("✅ Registration was successful");
 
     println!("Please provide the password (to login):");
-    let password = BigUint::from_bytes_be(args.password.trim().as_bytes());
+    let password = prompt_password("Password (to login): ")?;
+    let password = BigUint::from_bytes_be(password.trim().as_bytes());
 
     let k = ZKP::generate_random_number_below(&q);
     let (r1, r2) = zkp.compute_pair(&k);
 
     let request = AuthenticationChallengeRequest {
         user: username,
-        r1:   r1.to_bytes_be(),
-        r2:   r2.to_bytes_be(),
+        r1: r1.to_bytes_be(),
+        r2: r2.to_bytes_be(),
     };
 
-    let response = match client.create_authentication_challenge(request).await
-    {
+    let response = match client.create_authentication_challenge(request).await {
         Ok(x) => x,
-        Err(e) =>
-        {
-            return Err(format!("Could not request challenge to server {:?}", e));
+        Err(e) => {
+            return Err(anyhow!("Could not request challenge to server {:?}", e));
         }
     }
     .into_inner();
@@ -106,12 +95,10 @@ async fn main() -> Result<(), String>
         s: s.to_bytes_be(),
     };
 
-    let response = match client.verify_authentication(request).await
-    {
+    let response = match client.verify_authentication(request).await {
         Ok(x) => x,
-        Err(e) =>
-        {
-            return Err(format!("Could not verify authentication in server {:?}", e));
+        Err(e) => {
+            return Err(anyhow!("Could not verify authentication in server {:?}", e));
         }
     }
     .into_inner();
